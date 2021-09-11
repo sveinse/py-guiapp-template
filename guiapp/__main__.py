c31d5aa60d889c0eae3f227423388d7ac04869b9
Svein Seldal <sveinse@seldal.com>
2021-09-11 14:42:31 +0200

    Swap to qt5reactor

--- guiapp/__main__.py
+++ guiapp/__main__.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication
-import pyside2reactor
+import qt5reactor
 
 
 def main():
@@ -13,7 +13,7 @@ def main():
 
     # Install the pyside2 twisted reactor
     # This must be done before any other twisted imports
-    pyside2reactor.install()
+    qt5reactor.install()
 
     # This construct is required ensure twisted reactor is not loaded
     # before the above install
